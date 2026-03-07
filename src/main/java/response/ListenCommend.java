143411b727e7b1c4cd75dab10a3e0ccdeefefbde
carrot-41 <carrot41minecraft@gmail.com>
2026-03-07 20:57:44 +0900

    임포트문 최적화

--- src/main/java/response/ListenCommend.java
+++ src/main/java/response/ListenCommend.java
@@ -3,7 +3,6 @@ package response;
 import lombok.RequiredArgsConstructor;
 import my.bot.BotMain;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
