dc09b7fae83299e6efaab39f3964bf2ba759e883
carrot_41 <carrot41minecraft@gmail.com>
2026-03-07 22:45:52 +0900

    각 패키지 안에 Util폴로더 이동

---
+++ src/main/java/response/Util/HighestPermUtil.java
@@ -0,0 +1,20 @@
+package response.Util;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class HighestPermUtil {
+    //현재는 그냥 채팅 친 유저의 권한만 얻는거(추가할거 : 원하는 타겟의 권한과 이 봇의 권한을 얻어오기)
+    public static String GetHighestPerm(@NotNull MessageReceivedEvent messageReceivedEvent) {
+        return Objects.requireNonNull(messageReceivedEvent.getMember())
+                .getPermissions().stream()
+                .max(Comparator.comparingLong(Permission::getRawValue))
+                .map(Permission::getName)
+                .orElse("권한 없음");
+    }
+
+}
